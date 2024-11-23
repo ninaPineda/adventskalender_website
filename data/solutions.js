f08587204b60af521d693876cdd001745c6b6bfa
ninaPineda <ninaheinrich99.nh@gmail.com>
2024-11-23 12:42:59 +0100

    Create solutions.js

---
+++ data/solutions.js
@@ -0,0 +1,35 @@
+[
+  {
+    "riddle": 1,
+    "answers": [
+      "juri",
+      "juri war es",
+      "juri hat den keks gegessen",
+      "es war juri",
+      "juri ist schuld",
+      "juri war schuld",
+      "juri hat es getan",
+      "juri hat ihn gegessen",
+      "juri ist der schuldige",
+      "der schuldige ist juri",
+      "der täter ist juri"
+    ]
+  },
+  {
+    "riddle": 2,
+    "answers": [
+      "nikolausabend",
+      "niklausabend",
+      "nikolaus",
+      "niklaus",
+      "nikolaus-abend",
+      "niklaus-abend",
+      "nikolaus abend",
+      "niklaus abend",
+      "abend vom nikolaus",
+      "abend von nikolaus",
+      "nikolausabend mit geschenken",
+      "abend des nikolaus"
+    ]
+  }
+]
